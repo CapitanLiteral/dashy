c675343f31a98d2df7b6ddbf75719df446e43337
Aleix Cardona <aleix.cardona@scopely.com>
2023-02-04 02:27:26 +0100

    .

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,9 @@ docker stop DashyContainer
 rem Remove the old container
 docker rm DashyContainer
 
+rem Build dashy image
+docker build -t dashy .
+
 rem Start the new container with updated image
 docker run -d --name DashyContainer -p 6060:80 dashy
 
